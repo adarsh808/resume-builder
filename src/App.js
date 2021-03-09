ce73aad044f8b5da22f12dc331ad207dba58b3f1
Adarsh Dilu Mathew <adarshm@valorem.com>
2021-03-10 01:27:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router";
 import './App.css';
+import ResumeFormPage from './pages/resume-builder-form-page/resume-form-page';
+import Header from "./components/header/header.component";
+import ResumeTemplatePage from "./pages/resume-template/resume-template.component";
+import ResumeListPage from "./pages/resume-list/resume-list-page.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header></Header>
+      <div className="mt-5">
+        <Switch>
+          <Route exact path="/" component={ResumeFormPage} />
+          <Route exact path="/create-resume" component={ResumeFormPage} />
+          <Route exact path="/resumes" component={ResumeListPage} />
+          <Route exact path="/resume-view/:id" component={ResumeTemplatePage} />
+          <Route
+            exact
+            path="/*"
+            render={(props) => <Redirect to={{ pathname: "/" }} />}
+          />{" "}
+          <Route />
+        </Switch>
+      </div>
+    </React.Fragment>
   );
 }
 
